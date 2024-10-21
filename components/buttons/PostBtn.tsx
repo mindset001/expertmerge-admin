5745b67286f1cf4dfa9d6289044659cca57efa9a
shakiru102 <ashakiru53@gmail.com>
2024-10-21 23:49:25 +0100

    get

--- components/buttons/PostBtn.tsx
+++ components/buttons/PostBtn.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import Icon from '../icons/Icon'
 
 const PostBtn: FC<NavBarMenuListProps> = ({
-    icon,
+    
     text,
     className,
     onClick,
@@ -14,7 +14,7 @@ const PostBtn: FC<NavBarMenuListProps> = ({
     onClick={onClick}
     whileTap={{ scale: 0.9 }}
     className={`text-[14px] cursor-pointer text-center flex justify-center gap-3  items-center min-w-[59px] px-2 font-[500] text-[#667185] ${className}`}>
-      <Icon name={icon} />
+    
       <span>{text || "Home"}</span>
     </motion.div>
   )
